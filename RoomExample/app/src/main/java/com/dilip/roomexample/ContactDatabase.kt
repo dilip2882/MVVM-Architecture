498f1a9dfcf4860e1e7a945cfa531522e2dcb977
Dilip Patel <pateldilip2902@gmail.com>
2024-06-22 16:16:33 +0530

    implemented Singleton for only one instance

--- RoomExample/app/src/main/java/com/dilip/roomexample/ContactDatabase.kt
+++ RoomExample/app/src/main/java/com/dilip/roomexample/ContactDatabase.kt
@@ -1,10 +1,28 @@
 package com.dilip.roomexample
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Contact::class], version = 1)
 abstract class ContactDatabase : RoomDatabase() {
 
     abstract fun contactDao() :  ContactDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : ContactDatabase? = null
+
+        fun getDatabase(context: Context): ContactDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                        ContactDatabase::class.java,
+                        "contactDB").build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
--- RoomExample/app/src/main/java/com/dilip/roomexample/MainActivity.kt
+++ RoomExample/app/src/main/java/com/dilip/roomexample/MainActivity.kt
@@ -6,18 +6,22 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.room.Room
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var database: ContactDatabase
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = Room.databaseBuilder(applicationContext,
-            ContactDatabase::class.java,
-            "contactDB").build()
+        database = ContactDatabase.getDatabase(this)
+
+//        database = Room.databaseBuilder(applicationContext,
+//            ContactDatabase::class.java,
+//            "contactDB").build()
 
         GlobalScope.launch {
             database.contactDao().insertContact(Contact(0, "Dilip", "1111"))
